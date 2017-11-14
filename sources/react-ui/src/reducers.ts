ff42c030b4c6b0bc76f7a07544152a95421f0dca
Nicole Rauch <info@nicole-rauch.de>
2017-11-14 17:26:55 +0100

    Rewrote the game reducer to make handling the different actions plus the different game states more straightforward. Also, formatting.

--- sources/react-ui/src/reducers.ts
+++ sources/react-ui/src/reducers.ts
@@ -1,19 +1,19 @@
 import {combineReducers} from 'redux';
 
-import {ChainName, Game, GameState, Model, Player, PlayerType, SimpleMap, Stock} from './types';
+import {ChainName, GameState, Model, Player, PlayerType, SimpleMap} from './types';
 import {Action} from './actions';
 
-type Handler<Data> = (d:Data, a:Action) => Data;
+type Handler<Data> = (d: Data, a: Action) => Data;
 
 interface Handlers<Data> {
-    [key:string] : Handler<Data>;
+    [key: string]: Handler<Data>;
 }
 
-function createReducer<Data>(initialState : Data, handlers : Handlers<Data>) : Handler<Data> {
-    return (state : Data = initialState, action : Action = { type: 'InitialAction' }) =>
+function createReducer<Data>(initialState: Data, handlers: Handlers<Data>): Handler<Data> {
+    return (state: Data = initialState, action: Action = {type: 'InitialAction'}) =>
         handlers.hasOwnProperty(action.type) ?
             handlers[action.type](state, action) :
-            state
+            state;
 }
 
 const INITIAL_PLAYER = {
@@ -35,66 +35,63 @@ export const INITIAL_STATE: Model =
     };
 
 
-const strings = createReducer(INITIAL_STATE.strings, {
-});
+const strings = createReducer(INITIAL_STATE.strings, {});
 
 const displayMessages = createReducer(INITIAL_STATE.displayMessages, {
-    ['ShowMessages']: (data:boolean, action:Action) => true,
-    ['HideMessages']: (data:boolean, action:Action) => false
+    ['ShowMessages']: (data: boolean, action: Action) => true,
+    ['HideMessages']: (data: boolean, action: Action) => false
 });
 
-const errors = createReducer(INITIAL_STATE.errors, {
-});
+const errors = createReducer(INITIAL_STATE.errors, {});
 
-const domain = createReducer(INITIAL_STATE.domain, {
-});
+const domain = createReducer(INITIAL_STATE.domain, {});
 
-const wsServerUrl = createReducer(INITIAL_STATE.wsServerUrl, {
-});
+const wsServerUrl = createReducer(INITIAL_STATE.wsServerUrl, {});
 
 function isEmpty(str?: string) {
     return (!str || !str.trim());
 }
 
-const player = (name: string) : Player => ({
+const player = (name: string): Player => ({
     playerName: name,
     playerType: PlayerType.Human,
     tiles: [],
-    ownedStock: new SimpleMap<ChainName,number>(),
+    ownedStock: new SimpleMap<ChainName, number>(),
     ownedCash: 6000
 });
 
-const game = createReducer(INITIAL_STATE.game, {
-    ['SetName']: (game:GameState, action:Action) => {
-        switch(game.type) {
-            case 'Register':
-                switch (action.type) { // FIXME: stupid hack to work around untyped action
-                    case 'SetName':
-                        return { type: 'Register', player: player(action.name) };
-                }
-        }
-        return game;
-    },
-    ['RegisterPlayer']: (game:GameState, action:Action) => {
-        switch (game.type){
-            case 'Register':
-                if (!isEmpty(game.player.playerName)) {
+function game(game: GameState = INITIAL_STATE.game, action: Action = {type: 'InitialAction'}) {
+    switch (game.type) {
+
+        case 'Register':
+            switch (action.type) {
+                case 'SetName':
+                    return {type: 'Register', player: player(action.name)};
+                case 'RegisterPlayer':
+                    if (isEmpty(game.player.playerName)) {
+                        return game;
+                    }
                     // TODO send to backend: sendCommand model List
                     return {type: 'SelectGame', player: game.player, games: [], numPlayers: 1, numRobots: 5};
-                }
-                break;
-        }
-        return game;
-    },
-    ['CreateGame']: (game:GameState, action:Action) => {
-        switch(game.type) {
-            case 'SelectGame':
-                // TODO send to backend: sendCommand model (NewGame { numHumans = sg.numPlayers, numRobots = sg.numRobots })
-                return game;
-        }
-        return game;
+            }
+            return game;
+
+        case 'SelectGame':
+            switch (action.type) {
+                case 'CreateGame':
+                    // TODO send to backend: sendCommand model (NewGame { numHumans = sg.numPlayers, numRobots = sg.numRobots })
+                    return game;
+            }
+            return game;
+
+        case 'PlayGame':
+            return game;
+
+        case 'EndOfGame':
+            return game;
     }
-});
+    return game;
+}
 
 export default combineReducers({
     strings,
