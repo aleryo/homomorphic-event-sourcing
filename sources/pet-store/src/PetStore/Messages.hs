214d171c96288f576756dffee75150f7e7c36c49
Arnaud Bailly <arnaud.oqube@gmail.com>
2018-01-07 19:22:12 +0100

    defined simple Model for PetStore

--- sources/pet-store/src/PetStore/Messages.hs
+++ sources/pet-store/src/PetStore/Messages.hs
@@ -1,1 +1,8 @@
 module PetStore.Messages where
+
+data Input = CheckIn { petName :: String }
+           | CheckOut { petName :: String }
+           | ListPets
+
+data Output = Pets { pets :: [ String ] }
+            | Error { reason :: String }
--- sources/pet-store/src/PetStore/Model.hs
+++ sources/pet-store/src/PetStore/Model.hs
@@ -1,3 +1,54 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NamedFieldPuns        #-}
 module PetStore.Model where
 
-    
+import           Data.List   (delete)
+import           IOAutomaton
+
+data Input = CheckIn { petName :: String }
+           | CheckOut { petName :: String }
+           | ListPets
+           deriving (Eq, Show)
+
+data Output = PetCheckedIn { petName :: String }
+            | PetCheckedOut { petName :: String }
+            | Pets { pets :: [ String ] }
+            | Error { reason :: PetStoreError }
+           deriving (Eq, Show)
+
+data PetStoreError = PetAlreadyCheckedIn
+                   | PetAlreadyCheckedOut
+           deriving (Eq, Show)
+
+-- | Concrete state of a `PetStore`
+newtype PetStore = PetStore { hostedPets :: [ String ] }
+                 deriving (Eq, Show)
+
+-- | Abstract state of a `PetStore`
+-- It is either @Open@ or invalid @Sink@
+data PetStoreState = PetStoreOpen
+                   | Sink
+                     -- ^Special /sink/ state where all unhandled transitions endup
+                   deriving (Eq, Show)
+
+petStore :: Input
+         -> PetStore
+         -> (Maybe Output, PetStore)
+
+petStore CheckIn{petName}  store@PetStore{hostedPets}
+  | petName `notElem` hostedPets = (Just $ PetCheckedIn petName, PetStore $ petName:hostedPets)
+  | otherwise                    = (Just $ Error PetAlreadyCheckedIn, store)
+
+petStore CheckOut{petName}  store@PetStore{hostedPets}
+  | petName `notElem` hostedPets = (Just $ Error PetAlreadyCheckedOut, store)
+  | otherwise                    = (Just $ PetCheckedOut petName, PetStore $ delete petName hostedPets)
+
+petStore ListPets          s@PetStore{hostedPets} = (Just $ Pets hostedPets, s)
+
+instance IOAutomaton PetStore PetStoreState Input Output where
+  init       = PetStore []
+  sink       = const Sink
+  state      = const PetStoreOpen
+  update a _ = a
+  action     = petStore
