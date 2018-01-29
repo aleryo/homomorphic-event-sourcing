b8f3718cb1f94a7af7c38084ba586425725a1273
Nicole Rauch <info@nicole-rauch.de>
2018-01-30 00:05:40 +0100

    And now, all of a sudden, everything has become boilerplate... (considering adding some HOF...)

--- sources/pet-store-frontend/src-v2/actions.ts
+++ sources/pet-store-frontend/src-v2/actions.ts
@@ -1,96 +1,146 @@
-
-import * as ajax from "./ajaxcalls";
+import * as ajax from './ajaxcalls';
 
 import {Pet} from './types';
 
-export type Action
-    // frontend-only actions (events):
-    = { type: 'InitialAction' }
-    | { type: "PETS_LOADED", pets: Pet[] }
 
-    // frontend actions (events) that correspond to frontend-backend commands:
-    | { type: "PET_ADDED", addedPet: Pet }
-    | { type: "PET_SOLD", soldPet: Pet }
-    | { type: "USER_LOGGED_IN", addedUser: string }
-    | { type: "USER_LOGGED_OUT" }
+export type PetFromBackend = { petName: string, petType: string, petPrice: number }
+export type UserFromBackend = { userName: string }
+export type PaymentFromBackend = {}
+export type PetStoreErrorFromBackend = {}
 
-// backend events:
+export type DataFromBackend
+// events:
+    = { tag: 'PetAdded', pet: PetFromBackend }
+    | { tag: 'PetRemoved', pet: PetFromBackend }
+    | { tag: 'UserLoggedIn', user: UserFromBackend }
+    | { tag: 'AddedToBasket', user: UserFromBackend, pet: PetFromBackend }
+    | { tag: 'RemovedFromBasket', user: UserFromBackend, pet: PetFromBackend }
+    | { tag: 'CheckedOutBasket', user: UserFromBackend, payment: PaymentFromBackend, amount: number }
+    | { tag: 'UserLoggedOut', user: UserFromBackend }
+// answers:
+    | { tag: 'UserBasket', user: UserFromBackend, pets: PetFromBackend[] }
+    | { tag: 'Pets', pets: PetFromBackend[] }
+    | { tag: 'Error', reason: PetStoreErrorFromBackend }
+
+
+const transformPet = (petFromBackend: PetFromBackend) => ({
+    name: petFromBackend.petName,
+    species: petFromBackend.petType,
+    price: petFromBackend.petPrice
+});
+
+const transformPets = (petsFromBackend: PetFromBackend[]) => petsFromBackend.map(transformPet);
+
+const transformUser = (userFromBackend: UserFromBackend) => userFromBackend.userName;
+
+export function transformBackendDataToAction(dataFromBackend: DataFromBackend): Action {
+    switch (dataFromBackend.tag) {
+        case 'PetAdded':
+            return addPet(transformPet(dataFromBackend.pet));
+        case 'PetRemoved':
+            return {type: 'InitialAction'};
+        case 'UserLoggedIn':
+            return loginUser(transformUser(dataFromBackend.user));
+        case 'AddedToBasket':
+            return {type: 'InitialAction'};
+        case 'RemovedFromBasket':
+            return {type: 'InitialAction'};
+        case 'CheckedOutBasket':
+            return {type: 'InitialAction'};
+        case 'UserLoggedOut':
+            return logoutUser();
+        case 'UserBasket':
+            return {type: 'InitialAction'};
+        case 'Pets':
+            return updatePets(transformPets(dataFromBackend.pets));
+        case 'Error':
+            return {type: 'InitialAction'};
+    }
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////////////
 // ACTIONS
 
+export type Action
+    // frontend-only actions (events):
+    = { type: 'InitialAction' }
+    | { type: 'PETS_LOADED', pets: Pet[] }
+
+    // frontend actions (events) that correspond to frontend-backend commands:
+    | { type: 'PET_ADDED', addedPet: Pet }
+    | { type: 'PET_SOLD', soldPet: Pet }
+    | { type: 'USER_LOGGED_IN', addedUser: string }
+    | { type: 'USER_LOGGED_OUT' }
+
+// backend events:
+
+
 export function loadPets() {
-    return (dispatch:any) => {
-        ajax.fetchPets((pets:Pet[]) => {
-            dispatch(updatePets(pets));
-        });
+    return (dispatch: any) => {
+        ajax.fetchPets((d: DataFromBackend) => dispatch(transformBackendDataToAction(d)));
     };
 }
 
-export function updatePets(pets:Pet[]) : Action {
+export function updatePets(pets: Pet[]): Action {
     return {
-        type: "PETS_LOADED",
+        type: 'PETS_LOADED',
         pets: pets
-    }
+    };
 }
 
-export function addPet(pet:Pet) : Action {
+export function addPet(pet: Pet): Action {
     return {
-        type: "PET_ADDED",
+        type: 'PET_ADDED',
         addedPet: pet
-    }
-}
-
-
-export function addAndSubmitPet(pet:Pet) {
-    return (dispatch:any) => {
-
-        dispatch(addPet(pet));
-
-        ajax.submitPet(pet,(pets:Pet[]) => {
-            // dispatch(updatePets(pets));
-        });
     };
 }
 
 
-export function sellPet(pet:Pet) : Action {
+export function addAndSubmitPet(pet: Pet) {
+    return (dispatch: any) => {
+
+        // dispatch(addPet(pet));
+
+        ajax.submitPet(pet, (d: DataFromBackend) => dispatch(transformBackendDataToAction(d)));
+    };
+}
+
+
+export function sellPet(pet: Pet): Action {
     return {
-        type: "PET_SOLD",
+        type: 'PET_SOLD',
         soldPet: pet
-    }
+    };
 }
 
-export function loginUser(user:string) : Action {
+export function loginUser(user: string): Action {
     return {
-        type: "USER_LOGGED_IN",
+        type: 'USER_LOGGED_IN',
         addedUser: user
-    }
+    };
 }
 
-export function loginAndSubmitUser(user:string) {
-    return (dispatch:any) => {
+export function loginAndSubmitUser(user: string) {
+    return (dispatch: any) => {
 
         dispatch(loginUser(user));
 
-        ajax.submitLogin(user, dispatch);
+        ajax.submitLogin(user, (d: DataFromBackend) => dispatch(transformBackendDataToAction(d)));
     };
 }
 
-export function logoutUser() : Action {
-    return { type: "USER_LOGGED_OUT" };
+export function logoutUser(): Action {
+    return {type: 'USER_LOGGED_OUT'};
 }
 
 export function logoutAndSubmitUser() {
-    return (dispatch:any, state:any) => {
+    return (dispatch: any, state: any) => {
 
         const {user} = state;
 
         dispatch(logoutUser());
 
-        ajax.submitLogout(user,() => {
-            // dispatch(updatePets(pets));
-        });
+        ajax.submitLogout(user, (d: DataFromBackend) => dispatch(transformBackendDataToAction(d)));
     };
 }
--- sources/pet-store-frontend/src-v2/ajaxcalls.ts
+++ sources/pet-store-frontend/src-v2/ajaxcalls.ts
@@ -1,79 +1,36 @@
-import {Action, loginUser, logoutUser} from './actions';
+import {DataFromBackend} from './actions';
+import {Pet} from './types';
 
 const axios = require('axios');
 
-import {Pet} from './types';
 
-interface PetFromBackend {
-    petName: string,
-    petType: string,
-    petPrice: number
-}
-
-export function submitPet(pet: Pet, callback: any, url: string = '') {
+export function submitPet(pet: Pet, successCB: ((d: DataFromBackend) => void), url: string = '') {
     axios.post(url + '/pets',
         {
             petName: pet.name,
             petType: pet.species,
             petPrice: pet.price
-        }).then(
-        (response: any) => {
-            // callback(response.data);
-        }
-    ).catch(() => { console.log("ERROR!"); });
+        })
+        .then((response: { data: DataFromBackend }) => successCB(response.data))
+        .catch((error: string) => console.log('ERROR!', error));
 }
 
-function transformPets(dataFromBackend: any): Pet[] {
-    return dataFromBackend.pets.map((petFromBackend: PetFromBackend) => ({
-        name: petFromBackend.petName,
-        species: petFromBackend.petType,
-        price: petFromBackend.petPrice
-    }));
-}
 
 export function fetchPets(successCB: any, url: string = '') {
-    axios.get(url + '/pets', {
-        headers: {'Accept': 'application/json'},
-    }).then(
-        (response: any) => {
-            successCB(transformPets(response.data));
-        }).catch(
-        (error: string) => {
-            console.log('houston...', error);
-        });
+    axios.get(url + '/pets', {headers: {'Accept': 'application/json'}})
+        .then((response: { data: DataFromBackend }) => successCB(response.data))
+        .catch((error: string) => console.log('ERROR!', error));
 }
 
-function transformLogin(dataFromBackend: any): Action {
-    if(dataFromBackend.tag === "UserLoggedIn"){
-        // {"tag":"UserLoggedIn","user":{"userName":"Nicole"}}
-        return loginUser(dataFromBackend.user.userName);
-    } else {
-        return logoutUser();
-    }
+export function submitLogin(user: string, successCB: ((d: DataFromBackend) => void), url: string = '') {
+    axios.put(url + '/users/' + user)
+        .then((response: { data: DataFromBackend }) => successCB(response.data))
+        .catch((error: string) => console.log('ERROR!', error));
 }
 
-export function submitLogin(user: string, successCB: ((action:Action) => void), url: string = '') {
-    axios.put(url + '/users/' + user).then(
-        (response: any) => {
-            successCB(transformLogin(response.data));
-        }
-    ).catch(() => { console.log("ERROR!"); successCB(logoutUser()); });
-}
-
-function transformLogout(dataFromBackend: any): Action {
-    if(dataFromBackend.tag === "UserLoggedOut"){
-        // {"tag":"UserLoggedIn","user":{"userName":"Nicole"}}
-        return logoutUser();
-    } else {
-        return logoutUser(); // TODO FIXME
-    }
-}
-
-export function submitLogout(user: string, successCB: ((action:Action) => void), url: string = '') {
-    axios.delete(url + '/users/' + user).then(
-        (response: any) => {
-            successCB(transformLogout(response.data));
-        }
-    ).catch(() => { console.log("ERROR!"); });
+export function submitLogout(user: string, successCB: ((d: DataFromBackend) => void), url: string = '') {
+    axios.delete(url + '/users/' + user)
+        .then((response: { data: DataFromBackend }) => successCB(response.data))
+        .catch((error: string) => console.log('ERROR!', error));
 }
 
